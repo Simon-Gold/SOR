ed40451620d3d262ae5a8c277666b208777ec3f5
nik.nikitenko.dev@gmail.com <nik.nikitenko.dev@gmail.com>
2022-11-23 22:56:01 +0300

    frontend: modified dashboard, added search filed and users table

--- frontend/src/views/main/Dashboard.vue
+++ frontend/src/views/main/Dashboard.vue
@@ -5,13 +5,17 @@
         <div class="headline primary--text">Dashboard</div>
       </v-card-title>
       <v-card-text>
-        <div class="headline font-weight-light ma-5">Welcome {{greetedUser}}</div>
+        <template>
+          <v-text-field label="Search By First Name" v-model="firstName" @change="filterUserList"></v-text-field>
+        </template>
+        <v-data-table :headers="headers" :items="users">
+          <template slot="items" slot-scope="props">
+            <td>{{ props.item.email }}</td>
+            <td>{{ props.item.first_name }}</td>
+            <td>{{ props.item.last_name }}</td>
+          </template>
+        </v-data-table>
       </v-card-text>
-      <v-card-actions>
-        <v-btn to="/main/profile/view">View Profile</v-btn>
-        <v-btn to="/main/profile/edit">Edit Profile</v-btn>
-        <v-btn to="/main/profile/password">Change Password</v-btn>
-      </v-card-actions>
     </v-card>
   </v-container>
 </template>
@@ -20,9 +24,51 @@
 import { Component, Vue } from 'vue-property-decorator';
 import { Store } from 'vuex';
 import { readUserProfile } from '@/store/main/getters';
+import { readAdminUsers } from '@/store/admin/getters';
+import { dispatchGetUsers } from '@/store/admin/actions';
 
 @Component
 export default class Dashboard extends Vue {
+
+  public headers = [
+    {
+      text: 'Email',
+      sortable: true,
+      value: 'email',
+      align: 'left',
+    },
+    {
+      text: 'First Name',
+      sortable: true,
+      value: 'firs_name',
+      align: 'left',
+    },
+    {
+      text: 'Last Name',
+      sortable: true,
+      value: 'last_name',
+      align: 'left',
+    },
+  ];
+
+  public firstName = '';
+  public userList: any = [];
+
+  get users() {
+    this.userList = readAdminUsers(this.$store);
+    console.log(this.userList);
+
+    return this.userList;
+  }
+
+  public async mounted() {
+    await dispatchGetUsers(this.$store);
+  }
+
+  public filterUserList() {
+    // this.users = this.users.filter(u => u.first_name.includes(this.firstName));
+  }
+
   get greetedUser() {
     const userProfile = readUserProfile(this.$store);
     if (userProfile) {
