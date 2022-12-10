9e67c7de5e5358b4651bbbebf478840345d6c66e
nik.nikitenko.dev@gmail.com <nik.nikitenko.dev@gmail.com>
2022-12-10 23:21:55 +0300

    frontend: modified offender table and added pagination, modified sidebar responsivity for mobile

--- frontend/src/views/main/Dashboard.vue
+++ frontend/src/views/main/Dashboard.vue
@@ -6,9 +6,9 @@
           <div class="card-body">
             <h4 class="card-title text-primary"><i class="material-icons">search</i> Find Offender</h4>
             <div class="row mb-3">
-              <div class="col">
+              <div class="col-sm-12 col-md-4 col-lg-3">
                 <div class="field">
-                  <label class="label">Search By Last Name</label>
+                  <label class="label">Last Name</label>
                   <div class="control">
                     <input
                       type="text"
@@ -21,9 +21,9 @@
                   </div>
                 </div>
               </div>
-              <div class="col">
+              <div class="col-sm-12 col-md-4 col-lg-3">
                 <div class="field">
-                  <label class="label">Search By First Name</label>
+                  <label class="label">First Name</label>
                   <div class="control">
                     <input
                       type="text"
@@ -36,16 +36,16 @@
                   </div>
                 </div>
               </div>
-              <div class="col">
+              <div class="col-sm-12 col-md-4 col-lg-3">
                 <div class="field">
-                  <label class="label">Search By DOB</label>
+                  <label class="label">DOB</label>
                   <div class="control">
                     <input type="date" class="input" id="searchByDob" v-model="dob" @keyup.enter="searchOffenders" />
                   </div>
                 </div>
               </div>
-              <div class="col d-flex align-items-end">
-                <button class="button is-link" type="submit" @click="searchOffenders" style="max-width: 120px" :disabled="!lastName">
+              <div class="col-sm-12 col-md-4 col-lg-3 d-flex align-items-end">
+                <button class="button is-link mt-1" type="submit" @click="searchOffenders" style="max-width: 120px" :disabled="!lastName">
                   <i class="material-icons">search</i> Search
                 </button>
               </div>
@@ -58,32 +58,35 @@
                 </div>
               </div>
               <div class="col-12" v-if="showList">
-                <table class="table table-striped table-hover">
+                <table class="table table-striped table-bordered table-hover">
                   <thead>
                     <tr>
                       <th></th>
-                      <th>Offender Name</th>
-                      <th>Date of Birth</th>
-                      <th>Race</th>
-                      <th>Sex</th>
-                      <th>Height</th>
-                      <th>Weight</th>
-                      <th></th>
+                      <th>Offender</th>
+                      <th>Age</th>
+                      <th>Aliases</th>
+                      <th>Address</th>
                     </tr>
                   </thead>
                   <tbody>
-                    <tr v-for="(offender, index) in offenders">
-                      <td>{{ index + 1 }}</td>
-                      <td>{{ offender.names[0].first_name + " " + offender.names[0].middle + " " + offender.names[0].last_name }}</td>
-                      <td>{{ offender.dob.month + "/" + offender.dob.day + "/" + offender.dob.year }}</td>
-                      <td>{{ offender.demographic.race }}</td>
-                      <td>{{ offender.demographic.sex }}</td>
-                      <td>{{ offender.demographic.height }}</td>
-                      <td>{{ offender.demographic.weight }}</td>
-                      <td style="text-align: right">
+                    <tr v-for="(offender, index) in tempOffenders">
+                      <td style="vertical-align: middle; width: 130px">
                         <button class="button is-info is-small" @click="setOffender(offender)">
-                          <i class="material-icons">info</i> Go to Detail
+                          <i class="material-icons mr-1">logout</i> Go Detail
                         </button>
+                      </td>
+                      <td>
+                        <span class="d-block" v-if="offender.names[0].middle">{{ offender.names[0].middle }},</span>
+                        <span class="d-block">{{ offender.names[0].last_name + " " + offender.names[0].first_name }}</span>
+                      </td>
+                      <td>{{ offender.age }}</td>
+                      <td>
+                        <span class="d-block" v-if="offender.names.length > 1" v-for="name in offender.names.slice(1)">
+                          {{ name.first_name + " " + name.middle + " " + name.last_name }},
+                        </span>
+                      </td>
+                      <td>
+                        <span class="d-block" v-for="address in offender.addresses"> {{ address.line }}, </span>
                       </td>
                     </tr>
                   </tbody>
@@ -94,6 +97,24 @@
                   <i class="material-icons">info</i>
                   <span>The offenders to display was not found.</span>
                 </div>
+              </div>
+            </div>
+            <hr />
+            <div class="row">
+              <div class="col-12">
+                <nav aria-label="Page navigation example">
+                  <ul class="pagination justify-content-center">
+                    <li class="page-item" :class="{ disabled: currentPage == 1 }">
+                      <a class="page-link" @click="prevPage">Previous</a>
+                    </li>
+                    <li class="page-item" v-for="page in totalPage" :class="{ active: page == currentPage }">
+                      <a class="page-link" @click="setCurrentPage(page)">{{ page }}</a>
+                    </li>
+                    <li class="page-item" :class="{ disabled: currentPage == totalPage }">
+                      <a class="page-link" @click="nextPage">Next</a>
+                    </li>
+                  </ul>
+                </nav>
               </div>
             </div>
           </div>
@@ -109,28 +130,60 @@ import { Store } from "vuex";
 import { readOffenders } from "@/store/main/getters";
 import { dispatchGetOffenders, dispatchSearchOffenders } from "@/store/main/actions";
 import { IOffenders } from "@/interfaces";
+import { ref } from "vue";
 
 @Component
 export default class Dashboard extends Vue {
   firstName = "";
   lastName = "";
   dob = "";
-
   showList = false;
   spinner = false;
 
-  offenders: IOffenders[] | undefined;
+  offenders?: IOffenders[];
+  tempOffenders?: IOffenders[];
 
   query = "";
+  offenderPerPage = 3;
+  currentPage = 1;
+  totalPage? = 0;
+  totalOffender? = 0;
 
   async created() {
     this.offenders = [];
+    this.tempOffenders = [];
     this.showList = false;
     this.spinner = true;
     await dispatchGetOffenders(this.$store).then((data) => {
       this.spinner = false;
-      this.offenders = data;
+      this.offenders = data?.sort((a, b) => b.created_date.localeCompare(a.created_date, "en-US"));
       this.showList = this.offenders !== undefined && this.offenders.length > 0;
+      this.totalOffender = this.offenders?.length;
+      this.totalPage = Math.ceil((this.totalOffender ? this.totalOffender : 0) / this.offenderPerPage);
+      this.filter();
+    });
+  }
+
+  prevPage() {
+    this.currentPage--;
+    this.filter();
+  }
+
+  nextPage() {
+    this.currentPage++;
+    this.filter();
+  }
+
+  setCurrentPage(pageNumber: number) {
+    this.currentPage = pageNumber;
+    this.filter();
+  }
+
+  filter() {
+    this.tempOffenders = this.offenders?.filter((row, index) => {
+      let start = (this.currentPage - 1) * this.offenderPerPage;
+      let end = this.currentPage * this.offenderPerPage;
+      if (index >= start && index < end) return true;
     });
   }
 
--- frontend/src/views/main/Main.vue
+++ frontend/src/views/main/Main.vue
@@ -1,44 +1,54 @@
 <template>
   <div class="container-fluid overflow-hidden">
     <div class="row vh-100 overflow-auto">
-      <div class="col-12 col-sm-3 col-xl-2 px-sm-2 px-0 bg-dark d-flex sticky-top">
-        <div class="d-flex flex-sm-column flex-row flex-grow-1 align-items-center align-items-sm-start px-3 pt-2 text-white">
-          <div>
-            <a href="/" class="d-flex align-items-center pb-sm-3 mb-md-0 me-md-auto text-white text-decoration-none">
-              <span class="fs-5">S<span class="d-none d-sm-inline">OR</span></span>
+      <div :class="{ 'col-sm-12': width < 670, 'col-sm-3': width > 670 }" class="col-12 col-xl-2 px-sm-2 px-0 bg-dark d-flex sticky-top">
+        <div class="d-flex flex-sm-column flex-row flex-grow-1 align-items-center align-items-sm-start pl-3 pt-2 text-white">
+          <div style="width: 100%;">
+            <a
+              href="/"
+              class="d-flex align-items-center mb-md-0 me-md-auto text-white text-decoration-none"
+              style="border-bottom: 1px solid"
+            >
+              <span class="fs-5">Project SOR</span>
             </a>
             <ul
-              class="nav nav-pills flex-sm-column flex-row flex-nowrap flex-shrink-1 flex-sm-grow-0 flex-grow-1 mb-sm-auto mb-0 justify-content-center align-items-center align-items-sm-start"
+              :class="{ 'flex-sm-column': width > 670, 'align-items-sm-start': width > 670 }"
+              class="nav nav-pills flex-row flex-nowrap flex-shrink-1 flex-sm-grow-0 flex-grow-1 mb-sm-auto mb-0 justify-content-center align-items-center"
               id="menu"
             >
               <li class="nav-item">
                 <router-link class="nav-link px-sm-0 px-2" :to="{ name: 'main-dashboard' }" tag="button">
-                  <i class="material-icons mr-1">home</i><span class="ms-1 d-none d-sm-inline">Home</span>
+                  <i class="material-icons mr-1">home</i>
+                  <span :class="{ 'd-sm-inline': width > 670 }" class="ms-1 d-none">Home</span>
                 </router-link>
               </li>
               <li>
                 <router-link class="nav-link px-sm-0 px-2" :to="{ name: 'main-profile-view' }" tag="button">
-                  <i class="material-icons mr-1">person</i> <span class="ms-1 d-none d-sm-inline">Profile</span>
+                  <i class="material-icons mr-1">person</i>
+                  <span :class="{ 'd-sm-inline': width > 670 }" class="ms-1 d-none">Profile</span>
                 </router-link>
               </li>
               <li>
                 <router-link class="nav-link px-sm-0 px-2" :to="{ name: 'main-profile-edit' }" tag="button">
-                  <i class="material-icons mr-1">edit</i> <span class="ms-1 d-none d-sm-inline">Edit Profile</span>
+                  <i class="material-icons mr-1">edit</i>
+                  <span :class="{ 'd-sm-inline': width > 670 }" class="ms-1 d-none">Edit Profile</span>
                 </router-link>
               </li>
               <li>
                 <router-link class="nav-link px-sm-0 px-2" :to="{ name: 'main-profile-password' }" tag="button">
-                  <i class="material-icons mr-1">vpn_key</i> <span class="ms-1 d-none d-sm-inline">Change Password</span>
+                  <i class="material-icons mr-1">vpn_key</i>
+                  <span :class="{ 'd-sm-inline': width > 670 }" class="ms-1 d-none">Change Password</span>
                 </router-link>
               </li>
               <li class="dropdown" v-show="hasAdminAccess">
                 <a href="#" class="nav-link dropdown-toggle px-sm-0 px-1" id="dropdown" data-bs-toggle="dropdown" aria-expanded="false">
-                  <i class="material-icons mr-1">settings</i><span class="ms-1 d-none d-sm-inline">Admin</span>
+                  <i class="material-icons mr-1">settings</i>
+                  <span :class="{ 'd-sm-inline': width > 670 }" class="ms-1 d-none">Admin</span>
                 </a>
                 <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdown">
                   <li>
                     <router-link class="dropdown-item" :to="{ name: 'main-admin-users' }" tag="button">
-                      <i class="material-icons mr-1">group</i> <span class="ms-1 d-none d-sm-inline">Manage Users</span>
+                      <i class="material-icons mr-1">group</i> <span class="ms-1 d-sm-inline">Manage Users</span>
                     </router-link>
                   </li>
                   <li>
@@ -46,14 +56,15 @@
                   </li>
                   <li>
                     <router-link class="dropdown-item" :to="{ name: 'main-admin-users-create' }" tag="button">
-                      <i class="material-icons mr-1">person_add</i> <span class="ms-1 d-none d-sm-inline">Create User</span>
+                      <i class="material-icons mr-1">person_add</i> <span class="ms-1 d-sm-inline">Create User</span>
                     </router-link>
                   </li>
                 </ul>
               </li>
               <li>
                 <a class="nav-link px-sm-0 px-2" @click="logout">
-                  <i class="material-icons mr-1">close</i> <span class="ms-1 d-none d-sm-inline">Logout</span>
+                  <i class="material-icons mr-1">close</i>
+                  <span :class="{ 'd-sm-inline': width > 670 }" class="ms-1 d-none">Logout</span>
                 </a>
               </li>
             </ul>
@@ -86,6 +97,21 @@ const routeGuardMain = async (to, from, next) => {
 @Component
 export default class Main extends Vue {
   public appName = appName;
+
+  width = window.innerWidth;
+
+  created() {
+    window.addEventListener("resize", this.resizeHandler);
+  }
+
+  destroyed() {
+    window.removeEventListener("resize", this.resizeHandler);
+  }
+
+  resizeHandler(e) {
+    this.width = e.target.innerWidth;
+    // console.log(e.target.innerWidth);
+  }
 
   public beforeRouteEnter(to, from, next) {
     routeGuardMain(to, from, next);
