a50473127205e57e8da7603df8fbef282d0b806d
nik.nikitenko.dev@gmail.com <nik.nikitenko.dev@gmail.com>
2023-01-01 08:28:46 +0300

    delete old backend

--- backend/api/auth/schemas.py
+++
@@ -1,9 +0,0 @@
-from api import marsh as ma
-
-
-class TokenSchema(ma.Schema):
-    class Meta:
-        ordered = True
-
-    access_token = ma.String(required=True)
-    refresh_token = ma.String()
--- backend/boot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-flask db upgrade
-exec gunicorn -b :5000 --access-logfile - --error-logfile - project_sor:app
